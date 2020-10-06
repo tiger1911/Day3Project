7173c2ded974b317a8f634a4921bcf4ec8ca92c4
tiger1911 <mehmetatas@yahoo.com>
2020-10-06 21:59:20 +0200

    Added more awesome staff

--- src/test/java/AwesomeTest.java
+++ src/test/java/AwesomeTest.java
@@ -4,4 +4,10 @@ public class AwesomeTest {
     //green means it is under staging area
     //grey in .idea folder means it is ignored
     //when the color is normal, it means it is committed no changes
+
+    //I just pulled the new change from GitHub tha i did not have locally
+    //and now my project is in sync with remote repository
+
+    //Now i made more awesome changes and i am ABOUT to push this back to GitHub
+
 }
